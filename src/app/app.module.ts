424d7a6fa94853ebbe32504d840cd7a381b41c58
Sebastián Sánchez <sebatstian@gmail.com>
2020-02-05 12:21:43 -0300

    agregado constructor en AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Inject, PLATFORM_ID, APP_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,4 +24,14 @@ import { ContactoComponent } from './pages/contacto/contacto.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string
+  ) {
+    const platform = isPlatformBrowser(platformId)
+      ? 'in the browser'
+      : 'on the server';
+    console.log(`Corriendo: ${platformId} con: appId=${appId}`);
+  }
+}
